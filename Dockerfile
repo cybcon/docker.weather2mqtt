c0398d2460ddaf3a21e7aedf8ee5c62fde036375
Michael Oberdorf <info@oberdorf-itc.de>
2025-04-11 19:16:09 +0200

    move script, paste initial code frame and add library to translate weather codes

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
 FROM alpine:3.21.3
 
-LABEL maintainer="Hackergarden 2025-04-01"
+LABEL maintainer="Michael Oberdorf <info@oberdorf-itc.de>"
 LABEL site.local.program.version="0.1.0"
 
-ENV REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+ENV REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt \
+    MODE="current"
 
 COPY --chown=root:root /src /
 
 RUN apk upgrade --available --no-cache --update \
     && apk add --no-cache --update \
-       python3=3.12.9-r0 \
+       python3=3.12.10-r0 \
        py3-pip=24.3.1-r0 \
        ca-certificates=20241121-r1 \
     && pip3 install --no-cache-dir -r /requirements.txt --break-system-packages
@@ -20,4 +21,4 @@ WORKDIR /app
 
 # Start Process
 ENTRYPOINT ["python"]
-CMD ["-u", "/app/weather2mqtt.py"]
+CMD ["-u", "/app/bin/weather2mqtt.py"]
---
+++ src/app/bin/weather2mqtt.py
@@ -0,0 +1,46 @@
+"""
+###############################################################################
+# Tool to get weather information from the Open-Meteo API and publish it to MQTT\n
+# seeAlso: https://open-meteo.com/en/docs\n
+#------------------------------------------------------------------------------\n
+# This is a Hackergarden project, started during 50th Hackergarden 2025-04-01\n
+# at codecentric AG, Industriestraße 3, 70565 Stuttgart, Germany\n
+# seeAlso: https://www.hackergarten.net/\n
+# seeAlso: https://www.codecentric.de/standorte/stuttgart\n
+#------------------------------------------------------------------------------\n
+# Author: Michael Oberdorf\n
+# Date: 2025-04-11\n
+# Last modified by: Michael Oberdorf\n
+# Last modified at: 2025-04-11\n
+###############################################################################\n
+"""
+from lib.weather_codes import translate_weather_code
+#import openmeteo_requests
+#import requests_cache
+
+"""
+###############################################################################
+# F U N C T I O N S
+###############################################################################
+"""
+
+print(translate_weather_code(3))
+
+
+
+
+
+
+
+
+
+"""
+###############################################################################
+# M A I N
+###############################################################################
+"""
+
+if __name__ == "__main__":
+    """
+    Start the script
+    """
