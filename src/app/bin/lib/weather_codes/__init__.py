d243ca70b9c36a43ea5c56a967fe27e0a584bc53
Michael Oberdorf <info@oberdorf-itc.de>
2026-01-11 10:38:36 +0100

    Rewrite as python class and add support for multiple weather codes, add German as 1st translation

--- src/app/bin/lib/weather_codes/__init__.py
+++ src/app/bin/lib/weather_codes/__init__.py
@@ -16,26 +16,45 @@ __date__ = "2026-01-11"
 __version_info__ = ("1", "0", "0")
 __version__ = ".".join(__version_info__)
 
-__all__ = ["translate_weather_code"]
+__all__ = ["WeatherCodes"]
 
 import json
+import logging
 import os
 
-__default_weather_lang = "en"
 
-# identify the path to the translations folder
-__translations_path = os.path.join(__path__[0], "translations")
-# load the default language weather codes
-weather_code_translation_file = os.path.join(__translations_path, f"{__default_weather_lang}.json")
-with open(weather_code_translation_file, encoding="utf-8") as f:
-    __WEATHER_CODES = json.load(f)
-
-
-def translate_weather_code(code: int) -> str:
+class WeatherCodes:
     """
-    Translate weather code to human readable description
-
-    :param code: Weather code
-    :return: Human readable description
+    Class to translate weather codes to human readable description
     """
-    return __WEATHER_CODES.get(str(int(code)), "Unknown weather code")
+
+    __translations_path = os.path.join(__path__[0], "translations")
+
+    def __init__(self, language: str = "en"):
+        self.logger = logging.getLogger(__name__)
+
+        # identify the weather code translation file
+        weather_code_translation_file = os.path.join(self.__translations_path, f"{language}.json")
+        if not os.path.isfile(weather_code_translation_file):
+            self.logger.warning(
+                f"Weather code translation file for language '{language}' not found. Falling back to default language 'en'."
+            )
+            weather_code_translation_file = os.path.join(self.__translations_path, "en.json")
+
+        # load the weather code translations
+        with open(weather_code_translation_file, encoding="utf-8") as f:
+            self.weather_codes = json.load(f)
+
+    def translate(self, code: int) -> str:
+        """
+        Translate weather code to human readable description
+
+        :param code: Weather code
+        :return: Human readable description
+        """
+        # transform the code to int and str to fit to the json keys
+        code = str(int(code))
+        self.logger.debug(f"Translating weather code {code}")
+        translation = self.weather_codes.get(code, "Unknown weather code")
+        self.logger.debug(f"Weather code {code} translated to '{translation}'")
+        return translation
--- test/test_weather_codes.py
+++ test/test_weather_codes.py
@@ -19,16 +19,25 @@ __all__ = ["TestWeatherCodes"]
 
 import unittest
 
-from src.app.bin.lib.weather_codes import translate_weather_code
+from src.app.bin.lib.weather_codes import WeatherCodes
 
 
 class TestWeatherCodes(unittest.TestCase):
-    def test_translate_weather_code(self):
-        # Example test cases for translate_weather_code
-        self.assertEqual(translate_weather_code(0), "Clear sky")
-        self.assertEqual(translate_weather_code(1), "Mainly clear")
-        self.assertEqual(translate_weather_code(3), "Cloudy")
-        self.assertEqual(translate_weather_code(199), "Unknown weather code")
+    def test_translate(self):
+        # Test cases for default language (English)
+        self.assertEqual(WeatherCodes().translate(code=0), "Clear sky")
+        self.assertEqual(WeatherCodes().translate(code=1), "Mainly clear")
+
+        # Test cases for specific languages (English and German)
+        self.assertEqual(WeatherCodes(language="en").translate(code=3), "Cloudy")
+        self.assertEqual(WeatherCodes(language="en").translate(code=199), "Unknown weather code")
+        self.assertEqual(WeatherCodes(language="de").translate(code=45), "Nebel")
+        self.assertEqual(WeatherCodes(language="de").translate(code=66), "Leichter Eisregen")
+        self.assertEqual(WeatherCodes(language="de").translate(code=81), "Regenschauer")
+        self.assertEqual(WeatherCodes(language="de").translate(code=199), "Unknown weather code")
+
+        # Test case for unsupported language (fallback to English)
+        self.assertEqual(WeatherCodes(language="foo").translate(code=77), "Snow Grains")
 
 
 if __name__ == "__main__":
